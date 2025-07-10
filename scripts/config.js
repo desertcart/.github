5efc3f03d3b84e3bf35175375693cb8fa5740032
Mohammad Dohadwala <mohammad.dohad@desertcart.com>
2025-07-10 12:30:04 +0400

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -2,7 +2,7 @@ module.exports = {
   // Required permissions: repo:status, read:org, read:user
   // TODO: move this to github secrets
   GITHUB_TOKEN:
-    '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',
+    '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',
   ORG_NAME: 'desertcart',
   MAX_REPOS_DISPLAY: 10,
   MAX_CONTRIBUTORS_DISPLAY: 8,
